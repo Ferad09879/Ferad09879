1adc4235518da1ba2ff187e2a511b539bbdacd86
Ferad09879 <89129355+Ferad09879@users.noreply.github.com>
2021-08-19 10:29:20 +0200

    1234567890

--- schere stein papier .py
+++ schere stein papier .py
@@ -6,9 +6,14 @@ from colored import stylize
 # Begrüsung
 print(stylize("herzlich willkomen zu schre stein papier!", colored.fg(4)))
 
+
+computerpunkte = 0
+spielerpunkte = 0
+
 spilen = "JA"
 while spilen == "JA":
-
+  
+  
  # spiller wällen lassen
     spielerwarl = input("welle einen gegenstand: ")
 
@@ -19,31 +24,44 @@ while spilen == "JA":
      # if spielerwarl == "schre" and computerwal == "schre":
     if spielerwarl == "schere" and computerwal == "schere":
         print(stylize("Es ist Unenschiden", colored.fg(45)))
-        print("Aufwiedersehen") 
+        spielerpunkte = spielerpunkte + 1
+        computerpunkte = computerpunkte + 1
+        print("Nocchmal spillen? (dan schreib ja) ") 
     if spielerwarl == "stein" and computerwal == "stein":
         print(stylize("es ist Unenschiden", colored.fg(45)))
-        print("Aufwiedersehen") 
+        spielerpunkte = spielerpunkte + 1
+        computerpunkte = computerpunkte + 1
+        print("Nocchmal spillen? (dan schreib ja) ")  
     if spielerwarl == "papier" and computerwal == "papier":
         print(stylize("es ist Unenschiden", colored.fg(45)))
-        print("Aufwiedersehen") 
+        spielerpunkte = spielerpunkte + 1
+        computerpunkte = computerpunkte + 1
+        print("Nocchmal spillen? (dan schreib ja) ") 
     if spielerwarl == "stein" and computerwal == "schere":
         print(stylize("Du hast Gewonnen", colored.fg(221)))
-        print("Aufwiedersehen") 
+        spielerpunkte = spielerpunkte + 1
+        print("Nocchmal spillen? (dan schreib ja) ") 
     if spielerwarl == "schere" and computerwal == "stein":
         print(stylize("Du hast Ferloren", colored.fg(9)))
-        print("Aufwiedersehen") 
+        computerpunkte= computerpunkte + 1
+        print("Nocchmal spillen? (dan schreib ja) ") 
     if spielerwarl == "papier" and computerwal == "schere":
         print(stylize("Du hast Ferloren", colored.fg(9)))
-        print("Aufwiedersehen") 
+        computerpunkte= computerpunkte + 1
+        print("Nocchmal spillen? (dan schreib ja) ") 
     if spielerwarl == "stein" and computerwal == "papier":
         print(stylize("Du hast Ferloren", colored.fg(9)))
-        print("Aufwiedersehen") 
+        computerpunkte= computerpunkte + 1
+        print("Nocchmal spillen? (dan schreib ja) ") 
     if spielerwarl == "schere" and computerwal == "papier":
         print(stylize("Du hast Gewonnen", colored.fg(221)))
-        print("Aufwiedersehen") 
+        spielerpunkte = spielerpunkte + 1
+        print("Nocchmal spillen? (dan schreib ja) ") 
     if spielerwarl == "papier" and computerwal == "stein":
         print(stylize("Du hast Ferloren", colored.fg(9)))
-        print("Aufwiedersehen") 
-      
-        
+        computerpunkte= computerpunkte + 1
+        print("Nocchmal spillen? (dan schreib ja) ") 
+    
+    print("Punktestand: spiller", spielerpunkte, "computer", computerpunkte)
+
 
